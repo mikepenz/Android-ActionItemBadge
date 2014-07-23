2456e060f4a59f5c44d742ea7610c56d7da794f1
Mike Penz <mikepenz@gmail.com>
2014-07-23 21:58:33 +0200

    * initial commit

---
+++ app/src/main/java/com/tundem/actionitembadge/sample/MainActivity.java
@@ -0,0 +1,73 @@
+package com.tundem.actionitembadge.sample;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.joanzapata.android.iconify.Iconify;
+import com.tundem.aboutlibraries.Libs;
+import com.tundem.aboutlibraries.ui.LibsActivity;
+import com.tundem.actionitembadge.R;
+import com.tundem.actionitembadge.library.ActionItemBadge;
+
+
+public class MainActivity extends Activity {
+    private int badgeCount = 10;
+
+    private static final int SAMPLE2_ID = 34535;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        setTitle("");
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+
+        if (badgeCount > 0) {
+            ActionItemBadge.update(this, menu.findItem(R.id.item_samplebadge), Iconify.IconValue.fa_android, ActionItemBadge.BadgeStyle.RED, badgeCount);
+        } else {
+            ActionItemBadge.hide(menu.findItem(R.id.item_samplebadge));
+        }
+
+        new ActionItemBadge.Add().act(this).menu(menu).title(R.string.sample_2).itemDetails(0, SAMPLE2_ID, 1).showAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS).build(ActionItemBadge.BadgeStyle.BLUE_LARGE, 1);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_opensource) {
+            Intent i = new Intent(getApplicationContext(), LibsActivity.class);
+            i.putExtra(Libs.BUNDLE_FIELDS, Libs.toStringArray(R.string.class.getFields()));
+            i.putExtra(Libs.BUNDLE_LIBS, new String[]{"AndroidIconify"});
+            i.putExtra(Libs.BUNDLE_VERSION, true);
+            i.putExtra(Libs.BUNDLE_LICENSE, true);
+
+            i.putExtra(Libs.BUNDLE_TITLE, getString(R.string.action_opensource));
+
+            startActivity(i);
+            return true;
+        } else if (id == R.id.item_samplebadge) {
+            Toast.makeText(this, R.string.sample_3, Toast.LENGTH_SHORT).show();
+            badgeCount--;
+            invalidateOptionsMenu();
+            return true;
+        } else if (id == SAMPLE2_ID) {
+            Toast.makeText(this, R.string.sample_4, Toast.LENGTH_SHORT).show();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ library/src/main/java/com/tundem/actionitembadge/library/ActionItemBadge.java
@@ -0,0 +1,221 @@
+package com.tundem.actionitembadge.library;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.graphics.drawable.Drawable;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.joanzapata.android.iconify.IconDrawable;
+import com.joanzapata.android.iconify.Iconify;
+
+/**
+ * Created by mikepenz on 23.07.14.
+ */
+public class ActionItemBadge {
+    public enum BadgeStyle {
+        GREY(Style.DEFAULT, R.drawable.menu_grey_badge, R.layout.menu_badge),
+        RED(Style.DEFAULT, R.drawable.menu_red_badge, R.layout.menu_badge),
+        BLUE(Style.DEFAULT, R.drawable.menu_blue_badge, R.layout.menu_badge),
+        GREEN(Style.DEFAULT, R.drawable.menu_green_badge, R.layout.menu_badge),
+        PURPLE(Style.DEFAULT, R.drawable.menu_purple_badge, R.layout.menu_badge),
+        YELLOW(Style.DEFAULT, R.drawable.menu_yellow_badge, R.layout.menu_badge),
+        GREY_LARGE(Style.LARGE, R.drawable.menu_grey_badge_large, R.layout.menu_badge_large),
+        RED_LARGE(Style.LARGE, R.drawable.menu_red_badge_large, R.layout.menu_badge_large),
+        BLUE_LARGE(Style.LARGE, R.drawable.menu_blue_badge_large, R.layout.menu_badge_large),
+        GREEN_LARGE(Style.LARGE, R.drawable.menu_green_badge_large, R.layout.menu_badge_large),
+        PURPLE_LARGE(Style.LARGE, R.drawable.menu_purple_badge_large, R.layout.menu_badge_large),
+        YELLOW_LARGE(Style.LARGE, R.drawable.menu_yellow_badge_large, R.layout.menu_badge_large);
+
+        private Style style;
+        private int drawable;
+        private int layout;
+
+        private BadgeStyle(Style style, int drawable, int layout) {
+            this.style = style;
+            this.drawable = drawable;
+            this.layout = layout;
+        }
+
+        public Style getStyle() {
+            return style;
+        }
+
+        public int getDrawable() {
+            return drawable;
+        }
+
+        public int getLayout() {
+            return layout;
+        }
+
+
+        public enum Style {
+            DEFAULT(1),
+            LARGE(2);
+
+            private int style;
+
+            private Style(int style) {
+                this.style = style;
+            }
+
+            public int getStyle() {
+                return style;
+            }
+        }
+    }
+
+    public static class Add {
+        public Add() {
+
+        }
+
+        public Add(Activity activity, Menu menu, String title) {
+            this.activity = activity;
+            this.menu = menu;
+            this.title = title;
+        }
+
+        private Activity activity;
+
+        public Add act(Activity activity) {
+            this.activity = activity;
+            return this;
+        }
+
+        private Menu menu;
+
+        public Add menu(Menu menu) {
+            this.menu = menu;
+            return this;
+        }
+
+        private String title;
+
+        public Add title(String title) {
+            this.title = title;
+            return this;
+        }
+
+        public Add title(int resId) {
+            if (activity == null) {
+                throw new RuntimeException("Activity not set");
+            }
+
+            this.title = activity.getString(resId);
+            return this;
+        }
+
+        private Integer groupId;
+        private Integer itemId;
+        private Integer order;
+
+        public Add itemDetails(int groupId, int itemId, int order) {
+            this.groupId = groupId;
+            this.itemId = itemId;
+            this.order = order;
+            return this;
+        }
+
+        private Integer showAsAction;
+
+        public Add showAsAction(int showAsAction) {
+            this.showAsAction = showAsAction;
+            return this;
+        }
+
+        public Menu build(int badgeCount) {
+            return build(null, BadgeStyle.GREY_LARGE, badgeCount);
+        }
+
+        public Menu build(BadgeStyle style, int badgeCount) {
+            return build(null, style, badgeCount);
+        }
+
+        public Menu build(Iconify.IconValue icon, int badgeCount) {
+            return build(icon, BadgeStyle.GREY, badgeCount);
+        }
+
+        public Menu build(Iconify.IconValue icon, BadgeStyle style, int badgeCount) {
+            MenuItem item;
+            if (groupId != null && itemId != null && order != null) {
+                item = menu.add(groupId, itemId, order, title);
+            } else {
+                item = menu.add(title);
+            }
+
+            if (showAsAction != null) {
+                item.setShowAsAction(showAsAction);
+            }
+
+            item.setActionView(style.getLayout());
+            update(activity, item, icon, style, badgeCount);
+            return menu;
+        }
+    }
+
+    public static void update(final Activity act, final MenuItem menu, int badgeCount) {
+        update(act, menu, null, BadgeStyle.GREY_LARGE, badgeCount);
+    }
+
+    public static void update(final Activity act, final MenuItem menu, BadgeStyle style, int badgeCount) {
+        if (style.getStyle() != BadgeStyle.Style.LARGE) {
+            throw new RuntimeException("You are not allowed to call update without an icon on a Badge with default style");
+        }
+        update(act, menu, null, style, badgeCount);
+    }
+
+    public static void update(final Activity act, final MenuItem menu, Iconify.IconValue icon, int badgeCount) {
+        update(act, menu, icon, BadgeStyle.GREY, badgeCount);
+    }
+
+    public static void update(final Activity act, final MenuItem menu, Iconify.IconValue icon, BadgeStyle style, int badgeCount) {
+        View badge = menu.getActionView();
+
+        if (style.getStyle() == BadgeStyle.Style.DEFAULT) {
+            ImageView imageView = (ImageView) badge.findViewById(R.id.menu_badge_icon);
+            ActionItemBadge.setBackground(imageView, new IconDrawable(act, icon).colorRes(R.color.actionbar_text)
+                    .actionBarSize());
+
+            TextView textView = (TextView) badge.findViewById(R.id.menu_badge_text);
+            if (badgeCount < 0) {
+                textView.setVisibility(View.GONE);
+            } else {
+                textView.setText(String.valueOf(badgeCount));
+                textView.setBackgroundResource(style.getDrawable());
+            }
+        } else {
+            Button button = (Button) badge.findViewById(R.id.menu_badge_button);
+            button.setBackgroundResource(style.getDrawable());
+            button.setText(String.valueOf(badgeCount));
+        }
+
+        badge.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                act.onOptionsItemSelected(menu);
+            }
+        });
+
+        menu.setVisible(true);
+    }
+
+    public static void hide(MenuItem menu) {
+        menu.setVisible(true);
+    }
+
+    @SuppressLint("NewApi")
+    private static void setBackground(View v, Drawable d) {
+        int sdk = android.os.Build.VERSION.SDK_INT;
+        if (sdk < android.os.Build.VERSION_CODES.JELLY_BEAN) {
+            v.setBackgroundDrawable(d);
+        } else {
+            v.setBackground(d);
+        }
+    }
+}
