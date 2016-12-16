33b6f7e33120ec3f52bd82bfc03554074df9c4b7
Brian Doyle <brian@tf83.com>
2016-12-16 04:39:42 -0800

    Added missing update method/signature

--- library/src/main/java/com/mikepenz/actionitembadge/library/ActionItemBadge.java
+++ library/src/main/java/com/mikepenz/actionitembadge/library/ActionItemBadge.java
@@ -133,6 +133,9 @@ public class ActionItemBadge {
 
     }
 
+    public static void update(final Activity activity, final MenuItem menu, Drawable icon, BadgeStyle style, int badgeCount) {
+        update (activity, menu, icon, style, badgeCount, null);
+    }
 
     public static void update(final Activity activity, final MenuItem menu, Drawable icon, BadgeStyle style, int badgeCount, ActionItemBadgeListener listener) {
         if (badgeCount == Integer.MIN_VALUE) {
