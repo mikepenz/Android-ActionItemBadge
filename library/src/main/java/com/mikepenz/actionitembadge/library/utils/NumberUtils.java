d9f1014dbf667b136639a7da3c45b5bafe9178aa
Mike Penz <mikepenz@gmail.com>
2019-10-18 16:22:11 +0200

    * update all dependencies and everything * update to latest Android-iconics   * fix incompatiblity   * FIX https://github.com/mikepenz/Android-ActionItemBadge/issues/70

--- library/src/main/java/com/mikepenz/actionitembadge/library/utils/NumberUtils.java
+++ library/src/main/java/com/mikepenz/actionitembadge/library/utils/NumberUtils.java
@@ -10,6 +10,7 @@ import java.util.TreeMap;
 public class NumberUtils {
 
     private static final NavigableMap<Long, String> suffixes = new TreeMap<>();
+
     static {
         suffixes.put(1_000L, "k");
         suffixes.put(1_000_000L, "M");
